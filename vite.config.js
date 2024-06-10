2c22f7a0b3ee9b9cc2fba352a5fc9a35f2f45abe
BigByteinnovation <seo.bigbyte@gmail.com>
2024-06-10 10:58:54 +0530

    changes3

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react-router-dom'],
-  },
   build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
+    outDir: 'dist',
   },
 });
